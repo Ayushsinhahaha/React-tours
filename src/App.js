7e2fcb21d0b72fe7e589a93efb28e4001a920984
Ayush Sinha <iamayush02@gmail.com>
2022-11-03 16:07:51 +0530

    Completed the refresh button

--- src/App.js
+++ src/App.js
@@ -10,6 +10,11 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
 
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+
   const fetchTours = async () => {
     setLoading(true);
 
@@ -35,9 +40,19 @@ function App() {
       </main>
     );
   }
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>No Tours Left</h2>
+          <button onClick={fetchTours} className='btn'> Refresh</button>
+        </div>
+      </main>
+    );
+  }
   return (
     <main>
-      <Tours tours={tours} />
+      <Tours tours={tours} removeTour={removeTour} />
     </main>
   );
 }
